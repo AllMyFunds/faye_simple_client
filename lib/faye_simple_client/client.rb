3d97d83eba3fd7d60a399ea11cf5965cb953a27e
Wayne Robinson <wayne.robinson@gmail.com>
2015-09-29 18:03:43 +1000

    Deglobalise Client

--- lib/faye_simple_client/client.rb
+++ lib/faye_simple_client/client.rb
@@ -1,32 +1,49 @@
 module FayeSimpleClient
   class CustomError < StandardError; end
   class Client
+
     class << self
       attr_accessor :endpoint, :secret
 
       def client
-        @client ||= Faraday.new  url: endpoint do |c|
-          c.request :json
-          c.response :json, content_type: /\bjson$/
-          c.adapter Faraday.default_adapter
-        end
+        @client ||= new(endpoint, secret)
       end
 
       def push(channel, data)
-        response = client.post(nil, {
-          channel: channel,
-          data:    data,
-          ext:    { password: secret }
-        })
-        errors = response.body.inject([]) do |result, e|
-          result << e['error'] unless e['successful']
-          result
-        end
-        if errors.present?
-          raise CustomError.new(errors.compact.uniq.join(', '))
-        end
-        response
+        client.push(channel, data)
       end
     end
+
+    attr_reader :endpoint, :secret
+
+    def initialize(endpoint, secret)
+      @endpoint = endpoint
+      @secret = secret
+    end
+
+    def http
+      @http ||= Faraday.new(url: endpoint) do |c|
+        c.request :json
+        c.response :json, content_type: /\bjson$/
+        c.adapter Faraday.default_adapter
+      end
+    end
+
+    def push(channel, data)
+      response = http.post(nil, {
+        channel: channel,
+        data:    data,
+        ext:    { password: secret }
+      })
+      errors = response.body.inject([]) do |result, e|
+        result << e['error'] unless e['successful']
+        result
+      end
+      if errors.present?
+        raise CustomError.new(errors.compact.uniq.join(', '))
+      end
+      response
+    end
+
   end
 end
